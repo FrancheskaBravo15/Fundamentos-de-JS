321a2354d3c677f13e47092314a8efe6a45fabc7
Francheska <francheskabravo2019@gmail.com>
2025-06-11 20:16:40 -0500

    finalizando los fundamentos antes de la prueba

--- for.js
+++ for.js
@@ -1,5 +1,29 @@
-console.log("FOR");
+console.warn("-- For tradicional --");
 let marcasCelulares=['Samsung','Apple', 'Xiaomi', 'Infinix','Nokia'];
 for(let i=0; i<marcasCelulares.length; i++){
     console.log(marcasCelulares[i]);
 }
+
+//for in 
+console.warn("-- For in --");
+for(let i in marcasCelulares){
+    console.log(marcasCelulares[i]);
+}
+
+//for of
+console.warn("-- For of --");
+for(let marcaCelular of marcasCelulares){
+    console.log(marcaCelular);
+}
+
+//forEach
+console.warn("-- ForEach");
+marcasCelulares.forEach(function(marcaCelular, index){
+console.log(`${index}-${marcaCelular}`);
+});
+
+console.warn("-- forEach simple --");
+marcasCelulares.forEach(()=>{
+    console.log("Hola mundo");
+});
+
